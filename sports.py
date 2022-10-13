94fdd74d0db88dae5cba19cd200faa6cef069bf3
AlexFedus <83235408+AlexFedus@users.noreply.github.com>
2022-10-13 18:34:09 -0400

    formatting and organization. NCAA odds still broken

--- sports.py
+++ sports.py
@@ -1,67 +1,19 @@
 import requests
 import json
-import pprint
 
 
-# An api key is emailed to you when you sign up to a plan
-# Get a free API key at https://api.the-odds-api.com/
+
 API_KEY = "2d319702d293bc1f4ef00f963615da65"
-
 SPORT = 'americanfootball_ncaaf' #'upcoming' # use the sport_key from the /sports endpoint below, or use 'upcoming' to see the next 8 games across all sports
-
 REGIONS = 'us' # uk | us | eu | au. Multiple can be specified if comma delimited
-
 MARKETS = 'h2h,spreads' # h2h | spreads | totals. Multiple can be specified if comma delimited
-
 ODDS_FORMAT = 'american' # decimal | american
-
 DATE_FORMAT = 'iso' # iso | unix
 
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-#
-# First get a list of in-season sports
-#   The sport 'key' from the response can be used to get odds in the next request
-#
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
 
-sports_response = requests.get(
-    'https://api.the-odds-api.com/v4/sports?apiKey='+API_KEY
-)
-
-#(sports_response.json())
 
 
-"""
-if sports_response.status_code != 200:
-    print(f'Failed to get sports: status_code {sports_response.status_code}, response body {sports_response.text}')
-
-else:
-    data = json.loads(sports_response.text)
-   # pprint.pprint(data)
-
-    totaldata = len(data)-1
-    i = 0
-    while i < totaldata:
-        print(data[i]["title"])
-        i +=1
-
-
-   # group = data[0]["title"]
-   # title = data[1]["title"]
-
-  #  print(group, "  ", title)
-
-"""
-
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-#
-# Now get a list of live & upcoming games for the sport you want, along with odds for different bookmakers
-# This will deduct from the usage quota
-# The usage quota cost = [number of markets specified] x [number of regions specified]
-# For examples of usage quota costs, see https://the-odds-api.com/liveapi/guides/v4/#usage-quota-costs
-#
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
 sportchoice= (input("Choose NCAA or NFL:  "))
 
@@ -87,83 +39,147 @@ if odds_response.status_code != 200:
 
 else:
 
-    """
-    odds_json = odds_response.json()
-    print('Number of events:', len(odds_json))
-    print()
-    print(odds_json)
-    print()
-    """
-
     data = json.loads(odds_response.text)
-   # pprint.pprint(data)
-
     print()
 
     if SPORT=='americanfootball_nfl':
 
-        totaldata = len(data)
-        i = 0
-        while i < totaldata:
-            home =(data[i]["home_team"])
+        sportsbookchoice = input("Choose your sportsbook: 'Barstool, DraftKings, or FanDual':  ")
+        if (sportsbookchoice == "Barstool") or (sportsbookchoice == "barstool"):
+            sportsbookNum = 3
+        
+        elif (sportchoice == "Draftkings" or (sportsbookchoice == "Draftkings") or (sportsbookchoice == "draftkings")):
+            sportsbookNum = 16
+
+        elif (sportchoice == "FanDual" or (sportsbookchoice == "fandual") or (sportsbookchoice == "Fandual")):
+            sportsbookNum = 1
+
+        else:
+            sportsbookNum = -99
+            print("Sorry, please enter one of the three sportsbook listed. (Barstool, DraftKings, Fandual")
             
-            away = (data[i]["away_team"])
+        
+        if sportsbookNum != -99:
+            totaldata = len(data)
+            i = 0
+            while i < totaldata:
+                home = (data[i]["home_team"])
+                away = (data[i]["away_team"])
+                sportsbook= (data[i]['bookmakers'][sportsbookNum]['title'])
+                hometeam = (data[i]['bookmakers'][sportsbookNum]['markets'][0]['outcomes'][1]['name'])
+                awayteam = (data[i]['bookmakers'][sportsbookNum]['markets'][0]['outcomes'][0]['name'])
 
-            print(home, "at", away)
+                price=(data[i]['bookmakers'][sportsbookNum]['markets'][0]['outcomes'][0]['price'])
+                if int(price) > 0:
+                    price = "+"+str(price)
 
-            print("   Sportsbook: ",data[i]['bookmakers'][1]['title'])
-            print("   Moneyline:")
-            print("      ",data[i]['bookmakers'][1]['markets'][0]['outcomes'][0]['name'])
+                price2=(data[i]['bookmakers'][sportsbookNum]['markets'][0]['outcomes'][1]['price'])
+                if int(price2) > 0:
+                    price2 = "+"+str(price2)
 
-            price=(data[i]['bookmakers'][1]['markets'][0]['outcomes'][0]['price'])
-            if int(price) > 0:
-                price = "+"+str(price)
+                points=(data[i]['bookmakers'][sportsbookNum]['markets'][1]['outcomes'][0]['point'])
+                if int(points) > 0:
+                    points = "+"+str(points)
+                
+                points2=(data[i]['bookmakers'][sportsbookNum]['markets'][1]['outcomes'][1]['point'])
+                if int(points2) > 0:
+                    points2 = "+"+str(points2)
 
-            print('         ',price)   
+                odds = (data[i]['bookmakers'][sportsbookNum]['markets'][1]['outcomes'][0]['price'])
+                if int(odds) > 0:
+                    odds = "+"+str(odds)
 
-            print("      ",data[i]['bookmakers'][1]['markets'][0]['outcomes'][1]['name'])
+                odds2 = (data[i]['bookmakers'][sportsbookNum]['markets'][1]['outcomes'][1]['price'])
+                if int(odds2) > 0:
+                    odds2 = "+"+str(odds2)
+
+
+                print(home, "at", away)
+                print("   Sportsbook: " + sportsbook)
+                print("   Moneyline:")
+                print("      ",awayteam)
+                print('         ',price)   
+                print("      ",hometeam)
+                print('         ',price2) 
+                print("   Spread:")
+                print("      ", awayteam)
+                print("         Points: ", points)
+                print("         Odds: ", odds)
+                print("      ",hometeam)
+                print("         Points: ", points2)
+                print("         Odds: ", odds2)   
+                print()
+
+                i +=1
+                
+    elif SPORT=='americanfootball_ncaaf':
+        
+        sportsbookchoice = input("Choose your sportsbook: 'Barstool or DraftKings':  ")
+        if (sportsbookchoice == "Barstool") or (sportsbookchoice == "barstool"):
+            sportsbookNum = 7
+        
+        elif (sportchoice == "Draftkings" or (sportsbookchoice == "Draftkings") or (sportsbookchoice == "draftkings")):
+            sportsbookNum = 1
+
+        else:
+            sportsbookNum = -99
+            print("Sorry, please enter one of the three sportsbook listed. (Barstool, DraftKings, Fandual")
             
+        
+        if sportsbookNum != -99:
+            totaldata = len(data)
+            i = 0
+            while i < totaldata:
+                home = (data[i]["home_team"])
+                away = (data[i]["away_team"])
+                sportsbook= (data[i]['bookmakers'][sportsbookNum]['title'])
+                hometeam = (data[i]['bookmakers'][sportsbookNum]['markets'][0]['outcomes'][1]['name'])
+                awayteam = (data[i]['bookmakers'][sportsbookNum]['markets'][0]['outcomes'][0]['name'])
 
-            price2=(data[i]['bookmakers'][1]['markets'][0]['outcomes'][1]['price'])
-            if int(price2) > 0:
-                price2 = "+"+str(price2)
+                price=(data[i]['bookmakers'][sportsbookNum]['markets'][0]['outcomes'][0]['price'])
+                if int(price) > 0:
+                    price = "+"+str(price)
 
-            print('         ',price2) 
+                price2=(data[i]['bookmakers'][sportsbookNum]['markets'][0]['outcomes'][1]['price'])
+                if int(price2) > 0:
+                    price2 = "+"+str(price2)
 
-            print("   Spread:")
-            print("      ",data[i]['bookmakers'][1]['markets'][1]['outcomes'][0]['name'])
+                points=(data[i]['bookmakers'][sportsbookNum]['markets'][1]['outcomes'][0]['point'])
+                if int(points) > 0:
+                    points = "+"+str(points)
+                
+                points2=(data[i]['bookmakers'][sportsbookNum]['markets'][1]['outcomes'][1]['point'])
+                if int(points2) > 0:
+                    points2 = "+"+str(points2)
 
-            points=data[i]['bookmakers'][1]['markets'][1]['outcomes'][0]['point']
-            if int(points) > 0:
-                points = "+"+str(points)
+                odds = (data[i]['bookmakers'][sportsbookNum]['markets'][1]['outcomes'][0]['price'])
+                if int(odds) > 0:
+                    odds = "+"+str(odds)
 
-            print("         Points: ", points)
-
-            odds = data[i]['bookmakers'][1]['markets'][1]['outcomes'][0]['price']
-            if int(odds) > 0:
-                odds = "+"+str(odds)
-
-            print("         Odds: ", odds)
+                odds2 = (data[i]['bookmakers'][sportsbookNum]['markets'][1]['outcomes'][1]['price'])
+                if int(odds2) > 0:
+                    odds2 = "+"+str(odds2)
 
 
+                print(home, "at", away)
+                print("   Sportsbook: " + sportsbook)
+                print("   Moneyline:")
+                print("      ",awayteam)
+                print('         ',price)   
+                print("      ",hometeam)
+                print('         ',price2) 
+                print("   Spread:")
+                print("      ", awayteam)
+                print("         Points: ", points)
+                print("         Odds: ", odds)
+                print("      ",hometeam)
+                print("         Points: ", points2)
+                print("         Odds: ", odds2)   
+                print()
 
-            #Spread - home team
-            print("      ",data[i]['bookmakers'][1]['markets'][1]['outcomes'][1]['name'])
-
-            points2=data[i]['bookmakers'][1]['markets'][1]['outcomes'][1]['point']
-            if int(points2) > 0:
-                points2 = "+"+str(points2)
-
-            print("         Points: ", points2)
-
-            odds2 = data[i]['bookmakers'][1]['markets'][1]['outcomes'][1]['price']
-            if int(odds2) > 0:
-                odds2 = "+"+str(odds2)
-
-            print("         Odds: ", odds2)
-            
-            print()
-            i +=1
+                i +=1
+        
+        
 
 
 """"
