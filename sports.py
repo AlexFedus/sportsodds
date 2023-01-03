4898fba0e3d8a719b84d743128ef04e5bcff2a1d
AlexFedus <alex.fedus@gmail.com>
2023-01-03 17:58:26 -0500

    Added NFL Functionality

--- sports.py
+++ sports.py
@@ -8,7 +8,8 @@ class App(tk.Tk):
     def __init__(self):
         super().__init__()
 
-        API_KEY = "2d319702d293bc1f4ef00f963615da65"
+        #API_KEY = "2d319702d293bc1f4ef00f963615da65"
+        API_KEY = "74991ddf5f36ad6f09f21d4c7143aecd"   
         REGIONS = 'us' # uk | us | eu | au. Multiple can be specified if comma delimited
         MARKETS = 'h2h,spreads' # h2h | spreads | totals. Multiple can be specified if comma delimited
         ODDS_FORMAT = 'american' # decimal | american
@@ -148,29 +149,7 @@ class App(tk.Tk):
             i += 1
             ncaa_listbox.insert('end', "")
         
-        """
-        total_ncaa_data = len(ncaa_data)
-        print(total_ncaa_data)
-        i = 0
-        while i < total_ncaa_data:
-            home =(ncaa_data[i]["home_team"])
-            away =(ncaa_data[i]["away_team"])
-            ncaa_text = tk.Text(self.ncaa_frame)
-            ncaa_text.pack(fill='both', expand=True)
-            ncaa_text.insert('1.0', home + " " + away)
-            i += 1
-        """  
-
-            
-
-
-
-
-        """
-        ncaa_text = tk.Text(self.ncaa_frame)
-        ncaa_text.pack(fill='both', expand=True)
-        ncaa_text.insert('1.0', json.dumps(ncaa_data, indent=4))
-        """
+ 
         # Create the widgets for the NFL frame
         nfl_label = ttk.Label(self.nfl_frame, text='NFL Football')
         nfl_label.pack(pady=10)
@@ -190,10 +169,108 @@ class App(tk.Tk):
             }
         )
         
+        
+
         nfl_data = json.loads(nfl_response.text)
-        nfl_text = tk.Text(self.nfl_frame)
-        nfl_text.pack(fill='both', expand=True)
-        nfl_text.insert('1.0', json.dumps(nfl_data, indent=4))
+
+        
+        nfl_listbox = tk.Listbox(self.nfl_frame, width=75)
+        nfl_listbox.pack(fill='both', expand=True)
+
+        total_nfl_data = len(nfl_data)
+        i = 0
+        while i < total_nfl_data:
+            print(i)
+
+            hometeam = nfl_data[i]["home_team"]
+            awayteam = nfl_data[i]["away_team"]
+            matchupstring = awayteam + " VS " + hometeam
+            nfl_listbox.insert('end', matchupstring)
+            nfl_listbox.itemconfig('end', {'bg':'red'})
+
+            bookmakertotal = len(nfl_data[i])
+            b = 0
+            while b < bookmakertotal:
+                sportsbook = nfl_data[i]["bookmakers"][b]["title"]
+
+                try:
+                    spreadname = nfl_data[i]["bookmakers"][b]["markets"][1]["outcomes"][0]["name"]
+                    spreadprice = nfl_data[i]["bookmakers"][b]["markets"][1]["outcomes"][0]["point"]
+                    spreadodd = nfl_data[i]["bookmakers"][b]["markets"][1]["outcomes"][0]["price"]
+
+                    spreadname2 = nfl_data[i]["bookmakers"][b]["markets"][1]["outcomes"][1]["name"]
+                    spreadprice2 = nfl_data[i]["bookmakers"][b]["markets"][1]["outcomes"][1]["point"]
+                    spreadodd2 = nfl_data[i]["bookmakers"][b]["markets"][1]["outcomes"][1]["price"]
+
+                    if(spreadprice > 0):
+                        spreadprice = str("+"+str(spreadprice))
+                    if(spreadodd > 0):
+                        spreadodd = str("+"+str(spreadodd))
+                    if(spreadprice2 > 0):
+                        spreadprice2 = str("+"+str(spreadprice2))
+                    if(spreadodd2 > 0):
+                        spreadodd2 = str("+"+str(spreadodd2))
+                
+                except:
+                    spreadname = "null"
+                    spreadprice = "null"
+                    spreadodd = "null"
+
+                    spreadname2 = "null"
+                    spreadprice2 = "null"
+                    spreadodd2 = "null"
+
+
+                
+                
+                
+
+                moneyname = nfl_data[i]["bookmakers"][b]["markets"][0]["outcomes"][0]["name"]
+                moneyodd = nfl_data[i]["bookmakers"][b]["markets"][0]["outcomes"][0]["price"]
+                moneyname2 = nfl_data[i]["bookmakers"][b]["markets"][0]["outcomes"][1]["name"]
+                moneyodd2 = nfl_data[i]["bookmakers"][b]["markets"][0]["outcomes"][1]["price"]
+
+
+                if(moneyodd > 0):
+                    moneyodd = str("+"+str(moneyodd))
+                if(moneyodd2 > 0):
+                    moneyodd2 = str("+"+str(moneyodd2))
+
+                moneylinestring = moneyname + " " + str(moneyodd)
+                moneylinestring2 = moneyname2 + " " + str(moneyodd2)
+                nfl_listbox.insert('end', "        "+sportsbook)
+                nfl_listbox.itemconfig('end', {'bg':'lightgreen'})
+
+                nfl_listbox.insert('end', "                Spread -     " + str(spreadname))
+                nfl_listbox.itemconfig('end', {'bg':'lightblue'})
+
+                nfl_listbox.insert('end', "                                        Point    " + str(spreadprice))
+                nfl_listbox.itemconfig('end', {'bg':'lightblue'})
+
+                nfl_listbox.insert('end', "                                        Odds     " + str(spreadodd))
+                nfl_listbox.itemconfig('end', {'bg':'lightblue'})
+
+                nfl_listbox.insert('end', "                             " + str(spreadname2))
+                nfl_listbox.itemconfig('end', {'bg':'lightblue'})
+
+                nfl_listbox.insert('end', "                                        Point    " + str(spreadprice2))
+                nfl_listbox.itemconfig('end', {'bg':'lightblue'})
+
+                nfl_listbox.insert('end', "                                        Odds     " + str(spreadodd2))
+                nfl_listbox.itemconfig('end', {'bg':'lightblue'})
+
+                nfl_listbox.insert('end', "                Moneyline -  " + moneylinestring)
+                nfl_listbox.itemconfig('end', {'bg':'orange'})
+
+                nfl_listbox.insert('end', "                                      " + moneylinestring2)
+                nfl_listbox.itemconfig('end', {'bg':'orange'})
+
+                b += 1
+
+            i += 1
+            nfl_listbox.insert('end', "")
+
+    
 
         # Create the default frame and its widgets
         self.default_frame = tk.Frame(self)
@@ -213,7 +290,7 @@ class App(tk.Tk):
         self.default_frame.pack_forget()
 
     def show_nfl(self):
-        self.nfl_frame.pack()
+        self.nfl_frame.pack(fill="both", expand=True)
         self.ncaa_frame.pack_forget()
         self.default_frame.pack_forget()
 
