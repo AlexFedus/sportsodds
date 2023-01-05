5c29d1f720d0ea015794a00b0c3c8307ff8feaa5
AlexFedus <alex.fedus@gmail.com>
2023-01-04 19:11:18 -0500

    Added NHL and NBA functionality and increased font

--- sports.py
+++ sports.py
@@ -4,6 +4,7 @@ import requests
 import json
 from DateConverter import *
 import zoneinfo
+from tkinter import messagebox
 
 
 
@@ -27,6 +28,7 @@ class App(tk.Tk):
         y_coord = (screen_height // 2) - (window_height // 2)
         self.geometry(f'{window_width}x{window_height}+{x_coord}+{y_coord}')
         
+        
         # Use the 'clam' theme
         self.style = ttk.Style(self)
         self.style.theme_use('clam')
@@ -34,9 +36,12 @@ class App(tk.Tk):
         # Create the frames for each sport
         self.ncaa_frame = tk.Frame(self)
         self.nfl_frame = tk.Frame(self)
-        
+        self.nba_frame = tk.Frame(self)
+        self.nhl_frame = tk.Frame(self)
+
         # Create the widgets for the NCAA frame
         ncaa_label = ttk.Label(self.ncaa_frame, text='NCAA Football')
+        ncaa_label.config(font="Times 32")
         ncaa_label.pack(pady=10)
         back_button = ttk.Button(self.ncaa_frame, text='Back', command=self.show_default)
         back_button.pack(side='right')
@@ -63,7 +68,6 @@ class App(tk.Tk):
         total_ncaa_data = len(ncaa_data)
         i = 0
         while i < total_ncaa_data:
-            print(i)
 
             hometeam = ncaa_data[i]["home_team"]
             awayteam = ncaa_data[i]["away_team"]
@@ -78,7 +82,7 @@ class App(tk.Tk):
             ncaa_listbox.insert('end', str(convertDate(dateTime)) + " EST")
             ncaa_listbox.itemconfig('end', {'bg':'red'})
 
-            bookmakertotal = len(ncaa_data[i])
+            bookmakertotal = len(ncaa_data[i]["bookmakers"])
             b = 0
             while b < bookmakertotal:
                 sportsbook = ncaa_data[i]["bookmakers"][b]["title"]
@@ -159,10 +163,12 @@ class App(tk.Tk):
 
             i += 1
             ncaa_listbox.insert('end', "")
+            ncaa_listbox.config(font="Times 16")
         
  
         # Create the widgets for the NFL frame
         nfl_label = ttk.Label(self.nfl_frame, text='NFL Football')
+        nfl_label.config(font="Times 32")
         nfl_label.pack(pady=10)
         back_button = ttk.Button(self.nfl_frame, text='Back', command=self.show_default)
         back_button.pack(side='right')
@@ -191,7 +197,6 @@ class App(tk.Tk):
         total_nfl_data = len(nfl_data)
         i = 0
         while i < total_nfl_data:
-            print(i)
 
             hometeam = nfl_data[i]["home_team"]
             awayteam = nfl_data[i]["away_team"]
@@ -204,7 +209,7 @@ class App(tk.Tk):
             nfl_listbox.insert('end', str(convertDate(dateTime)) + " EST")
             nfl_listbox.itemconfig('end', {'bg':'red'})
 
-            bookmakertotal = len(nfl_data[i])
+            bookmakertotal = len(nfl_data[i]["bookmakers"])
             b = 0
             while b < bookmakertotal:
                 sportsbook = nfl_data[i]["bookmakers"][b]["title"]
@@ -285,17 +290,284 @@ class App(tk.Tk):
 
             i += 1
             nfl_listbox.insert('end', "")
+            nfl_listbox.config(font="Times 16")
+
+    
+        # Create the widgets for the NBA frame
+        nba_label = ttk.Label(self.nba_frame, text='NBA Basketball')
+        nba_label.config(font="Times 32")
+        nba_label.pack(pady=10)
+        back_button = ttk.Button(self.nba_frame, text='Back', command=self.show_default)
+        back_button.pack(side='right')
+
+        # Get the NFL data and display it
+
+        nba_response = requests.get(
+            f'https://api.the-odds-api.com/v4/sports/basketball_nba/odds',
+            params={
+                'api_key': API_KEY,
+                'regions': REGIONS,
+                'markets': MARKETS,
+                'oddsFormat': ODDS_FORMAT,
+                'dateFormat': DATE_FORMAT,
+            }
+        )
+        
+        
+
+        nba_data = json.loads(nba_response.text)
+
+        
+        nba_listbox = tk.Listbox(self.nba_frame, width=75)
+        nba_listbox.pack(fill='both', expand=True)
+
+        total_nba_data = len(nba_data)
+        i = 0
+        while i < total_nba_data:
+
+            hometeam = nba_data[i]["home_team"]
+            awayteam = nba_data[i]["away_team"]
+            matchupstring = awayteam + " VS " + hometeam
+            nba_listbox.insert('end', matchupstring)
+            nba_listbox.itemconfig('end', {'bg':'red'})
+
+
+            dateTime = nba_data[i]["commence_time"]
+            nba_listbox.insert('end', str(convertDate(dateTime)) + " EST")
+            nba_listbox.itemconfig('end', {'bg':'red'})
+
+            bookmakertotal = len(nba_data[i]["bookmakers"])
+            b = 0
+            while b < bookmakertotal:
+
+                sportsbook = nba_data[i]["bookmakers"][b]["title"]
+
+                try:
+                    spreadname = nba_data[i]["bookmakers"][b]["markets"][1]["outcomes"][0]["name"]
+                    spreadprice = nba_data[i]["bookmakers"][b]["markets"][1]["outcomes"][0]["point"]
+                    spreadodd = nba_data[i]["bookmakers"][b]["markets"][1]["outcomes"][0]["price"]
+
+                    spreadname2 = nba_data[i]["bookmakers"][b]["markets"][1]["outcomes"][1]["name"]
+                    spreadprice2 = nba_data[i]["bookmakers"][b]["markets"][1]["outcomes"][1]["point"]
+                    spreadodd2 = nba_data[i]["bookmakers"][b]["markets"][1]["outcomes"][1]["price"]
+
+                    if(spreadprice > 0):
+                        spreadprice = str("+"+str(spreadprice))
+                    if(spreadodd > 0):
+                        spreadodd = str("+"+str(spreadodd))
+                    if(spreadprice2 > 0):
+                        spreadprice2 = str("+"+str(spreadprice2))
+                    if(spreadodd2 > 0):
+                        spreadodd2 = str("+"+str(spreadodd2))
+                
+                except:
+                    spreadname = "null"
+                    spreadprice = "null"
+                    spreadodd = "null"
+
+                    spreadname2 = "null"
+                    spreadprice2 = "null"
+                    spreadodd2 = "null"
+
+
+                
+                
+                
+
+                moneyname = nba_data[i]["bookmakers"][b]["markets"][0]["outcomes"][0]["name"]
+                moneyodd = nba_data[i]["bookmakers"][b]["markets"][0]["outcomes"][0]["price"]
+                moneyname2 = nba_data[i]["bookmakers"][b]["markets"][0]["outcomes"][1]["name"]
+                moneyodd2 = nba_data[i]["bookmakers"][b]["markets"][0]["outcomes"][1]["price"]
+
+
+                if(moneyodd > 0):
+                    moneyodd = str("+"+str(moneyodd))
+                if(moneyodd2 > 0):
+                    moneyodd2 = str("+"+str(moneyodd2))
+
+                moneylinestring = moneyname + " " + str(moneyodd)
+                moneylinestring2 = moneyname2 + " " + str(moneyodd2)
+                nba_listbox.insert('end', "        "+sportsbook)
+                nba_listbox.itemconfig('end', {'bg':'lightgreen'})
+
+                nba_listbox.insert('end', "                Spread -     " + str(spreadname))
+                nba_listbox.itemconfig('end', {'bg':'lightblue'})
+
+                nba_listbox.insert('end', "                                        Point    " + str(spreadprice))
+                nba_listbox.itemconfig('end', {'bg':'lightblue'})
+
+                nba_listbox.insert('end', "                                        Odds     " + str(spreadodd))
+                nba_listbox.itemconfig('end', {'bg':'lightblue'})
+
+                nba_listbox.insert('end', "                             " + str(spreadname2))
+                nba_listbox.itemconfig('end', {'bg':'lightblue'})
+
+                nba_listbox.insert('end', "                                        Point    " + str(spreadprice2))
+                nba_listbox.itemconfig('end', {'bg':'lightblue'})
+
+                nba_listbox.insert('end', "                                        Odds     " + str(spreadodd2))
+                nba_listbox.itemconfig('end', {'bg':'lightblue'})
+
+                nba_listbox.insert('end', "                Moneyline -  " + moneylinestring)
+                nba_listbox.itemconfig('end', {'bg':'orange'})
+
+                nba_listbox.insert('end', "                                      " + moneylinestring2)
+                nba_listbox.itemconfig('end', {'bg':'orange'})
+
+                b += 1
+
+            i += 1
+            nba_listbox.insert('end', "")
+            nba_listbox.config(font="Times 16")
 
     
 
+
+        # Create the widgets for the NHL frame
+        nhl_label = ttk.Label(self.nhl_frame, text='NHL Hockey')
+        nhl_label.config(font="Times 32")
+        nhl_label.pack(pady=10)
+        back_button = ttk.Button(self.nhl_frame, text='Back', command=self.show_default)
+        back_button.pack(side='right')
+
+        # Get the NFL data and display it
+
+        nhl_response = requests.get(
+            f'https://api.the-odds-api.com/v4/sports/icehockey_nhl/odds',
+            params={
+                'api_key': API_KEY,
+                'regions': REGIONS,
+                'markets': MARKETS,
+                'oddsFormat': ODDS_FORMAT,
+                'dateFormat': DATE_FORMAT,
+            }
+        )
+        
+        
+
+        nhl_data = json.loads(nhl_response.text)
+
+        
+        nhl_listbox = tk.Listbox(self.nhl_frame, width=75)
+        nhl_listbox.pack(fill='both', expand=True)
+
+        total_nhl_data = len(nhl_data)
+        i = 0
+        while i < total_nhl_data:
+
+            hometeam = nhl_data[i]["home_team"]
+            awayteam = nhl_data[i]["away_team"]
+            matchupstring = awayteam + " VS " + hometeam
+            nhl_listbox.insert('end', matchupstring)
+            nhl_listbox.itemconfig('end', {'bg':'red'})
+
+
+            dateTime = nhl_data[i]["commence_time"]
+            nhl_listbox.insert('end', str(convertDate(dateTime)) + " EST")
+            nhl_listbox.itemconfig('end', {'bg':'red'})
+
+            bookmakertotal = len(nhl_data[i]["bookmakers"])
+            b = 0
+            while b < bookmakertotal:
+
+                sportsbook = nhl_data[i]["bookmakers"][b]["title"]
+
+                try:
+                    spreadname = nhl_data[i]["bookmakers"][b]["markets"][1]["outcomes"][0]["name"]
+                    spreadprice = nhl_data[i]["bookmakers"][b]["markets"][1]["outcomes"][0]["point"]
+                    spreadodd = nhl_data[i]["bookmakers"][b]["markets"][1]["outcomes"][0]["price"]
+
+                    spreadname2 = nhl_data[i]["bookmakers"][b]["markets"][1]["outcomes"][1]["name"]
+                    spreadprice2 = nhl_data[i]["bookmakers"][b]["markets"][1]["outcomes"][1]["point"]
+                    spreadodd2 = nhl_data[i]["bookmakers"][b]["markets"][1]["outcomes"][1]["price"]
+
+                    if(spreadprice > 0):
+                        spreadprice = str("+"+str(spreadprice))
+                    if(spreadodd > 0):
+                        spreadodd = str("+"+str(spreadodd))
+                    if(spreadprice2 > 0):
+                        spreadprice2 = str("+"+str(spreadprice2))
+                    if(spreadodd2 > 0):
+                        spreadodd2 = str("+"+str(spreadodd2))
+                
+                except:
+                    spreadname = "null"
+                    spreadprice = "null"
+                    spreadodd = "null"
+
+                    spreadname2 = "null"
+                    spreadprice2 = "null"
+                    spreadodd2 = "null"
+
+
+                
+                
+                
+
+                moneyname = nhl_data[i]["bookmakers"][b]["markets"][0]["outcomes"][0]["name"]
+                moneyodd = nhl_data[i]["bookmakers"][b]["markets"][0]["outcomes"][0]["price"]
+                moneyname2 = nhl_data[i]["bookmakers"][b]["markets"][0]["outcomes"][1]["name"]
+                moneyodd2 = nhl_data[i]["bookmakers"][b]["markets"][0]["outcomes"][1]["price"]
+
+
+                if(moneyodd > 0):
+                    moneyodd = str("+"+str(moneyodd))
+                if(moneyodd2 > 0):
+                    moneyodd2 = str("+"+str(moneyodd2))
+
+                moneylinestring = moneyname + " " + str(moneyodd)
+                moneylinestring2 = moneyname2 + " " + str(moneyodd2)
+                nhl_listbox.insert('end', "        "+sportsbook)
+                nhl_listbox.itemconfig('end', {'bg':'lightgreen'})
+
+                nhl_listbox.insert('end', "                Spread -     " + str(spreadname))
+                nhl_listbox.itemconfig('end', {'bg':'lightblue'})
+
+                nhl_listbox.insert('end', "                                        Point    " + str(spreadprice))
+                nhl_listbox.itemconfig('end', {'bg':'lightblue'})
+
+                nhl_listbox.insert('end', "                                        Odds     " + str(spreadodd))
+                nhl_listbox.itemconfig('end', {'bg':'lightblue'})
+
+                nhl_listbox.insert('end', "                             " + str(spreadname2))
+                nhl_listbox.itemconfig('end', {'bg':'lightblue'})
+
+                nhl_listbox.insert('end', "                                        Point    " + str(spreadprice2))
+                nhl_listbox.itemconfig('end', {'bg':'lightblue'})
+
+                nhl_listbox.insert('end', "                                        Odds     " + str(spreadodd2))
+                nhl_listbox.itemconfig('end', {'bg':'lightblue'})
+
+                nhl_listbox.insert('end', "                Moneyline -  " + moneylinestring)
+                nhl_listbox.itemconfig('end', {'bg':'orange'})
+
+                nhl_listbox.insert('end', "                                      " + moneylinestring2)
+                nhl_listbox.itemconfig('end', {'bg':'orange'})
+
+                b += 1
+
+            i += 1
+            nhl_listbox.insert('end', "")
+            nhl_listbox.config(font="Times 16")
+
+    
+
+
+
+
         # Create the default frame and its widgets
         self.default_frame = tk.Frame(self)
-        default_label = ttk.Label(self.default_frame, text='Choose a sport:')
+        default_label = ttk.Label(self.default_frame, text='Choose a sport:', font=('Times 32'))
         default_label.pack(pady=10)
         ncaa_button = ttk.Button(self.default_frame, text='NCAA Football', command=self.show_ncaa)
         nfl_button = ttk.Button(self.default_frame, text='NFL Football', command=self.show_nfl)
-        ncaa_button.pack(side='left')
-        nfl_button.pack(side='left')
+        nba_button = ttk.Button(self.default_frame, text='NBA Basketball', command=self.show_nba)
+        nhl_button = ttk.Button(self.default_frame, text='NHL Hockey', command=self.show_nhl)
+
+        ncaa_button.pack(side='top', pady = 15)
+        nfl_button.pack(side='top', pady = 15)
+        nba_button.pack(side='top', pady = 15)
+        nhl_button.pack(side='top', pady = 15)
 
         # Show the default frame by default
         self.show_default()
@@ -303,17 +575,37 @@ class App(tk.Tk):
     def show_ncaa(self):
         self.ncaa_frame.pack(fill='both', expand=True)
         self.nfl_frame.pack_forget()
+        self.nba_frame.pack_forget()
+        self.nhl_frame.pack_forget()
         self.default_frame.pack_forget()
 
     def show_nfl(self):
         self.nfl_frame.pack(fill="both", expand=True)
         self.ncaa_frame.pack_forget()
+        self.nba_frame.pack_forget()
+        self.nhl_frame.pack_forget()
+        self.default_frame.pack_forget()
+
+    def show_nba(self):
+        self.nba_frame.pack(fill="both", expand = True)
+        self.ncaa_frame.pack_forget()
+        self.nfl_frame.pack_forget()
+        self.nhl_frame.pack_forget()
+        self.default_frame.pack_forget()
+
+    def show_nhl(self):
+        self.nhl_frame.pack(fill="both", expand = True)
+        self.ncaa_frame.pack_forget()
+        self.nfl_frame.pack_forget()
+        self.nba_frame.pack_forget()
         self.default_frame.pack_forget()
 
     def show_default(self):
         self.default_frame.pack()
         self.ncaa_frame.pack_forget()
         self.nfl_frame.pack_forget()
+        self.nba_frame.pack_forget()
+        self.nhl_frame.pack_forget()
 
 app = App()
 app.mainloop()
